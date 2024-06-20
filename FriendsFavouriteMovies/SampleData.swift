4b67d84f4b254ad4ca97b48111e97d932d2a864c
Tony Sharples <tonysharples22@me.com>
2024-06-20 10:45:01 +0100

    Create SampleData class

---
+++ FriendsFavouriteMovies/SampleData.swift
@@ -0,0 +1,33 @@
+//
+//  SampleData.swift
+//  FriendsFavouriteMovies
+//
+//  Created by Tony Sharples on 20/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+class SampleData {
+    static let shared = SampleData()
+    
+    let modelContainer: ModelContainer
+    
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    private init() {
+        let schema = Schema([
+            Movie.self,
+        ])
+        
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }    }
+}
