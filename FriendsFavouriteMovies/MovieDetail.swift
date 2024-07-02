c06dd83b8052857dddb7ec2ae998aae76d99b057
Tony Sharples <tonysharples22@me.com>
2024-07-02 11:49:32 +0100

    Create confirm and cancel actions in MovieDetail view

--- FriendsFavouriteMovies/MovieDetail.swift
+++ FriendsFavouriteMovies/MovieDetail.swift
@@ -10,17 +10,35 @@ import SwiftUI
 struct MovieDetail: View {
     @Bindable var movie: Movie
     
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
     var body: some View {
         Form {
             TextField("Movie title", text: $movie.title)
             DatePicker("Release Date", selection: $movie.releaseDate, displayedComponents: .date)
         }
         .navigationTitle("Movie")
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                Button("Done") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    modelContext.delete(movie)
+                    dismiss()
+                }
+            }
+        }
     }
 }
 
 #Preview {
     NavigationStack {
         MovieDetail(movie: SampleData.shared.movie)
+            .modelContainer(SampleData.shared.modelContainer)
     }
 }
